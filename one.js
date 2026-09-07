e7965458b40353e824684a90f897ed3b37875287
ramabharathi0028 <159110216+ramabharathi0028@users.noreply.github.com>
2026-09-07 12:25:43 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,7 @@
+
+    let name=prompt("enter ur name")
+    console.log("hi " +name+ " welcomew")
+
+    let age=prompt("enter ur age")
+    console.log("hi " + name + " your age is " +age);
+    console.log(`hi ${name} your age is ${age}`);
