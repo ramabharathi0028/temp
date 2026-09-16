cda24175883ad1a8b7e5dde778b3f5306070c73e
ramabharathi0028 <159110216+ramabharathi0028@users.noreply.github.com>
2026-09-16 11:15:25 +0530

    Add files via upload

---
+++ for.js
@@ -0,0 +1,212 @@
+// let n= 5;
+// let sum=0;
+// let out="";
+// for(i=1;i<=n;i++){
+//     out+=i;
+//     if(i<n){
+//         out+="+"
+//     }
+//     sum+=i;
+// }
+// console.log(out+"="+sum)
+
+// let s= 5;
+// let out1="";
+// for(i=1;i<=n;i+2){
+//     out1+=i;
+//     if(i<n){
+//         out1+="+"
+//     }
+    
+// }
+// console.log(out1)
+
+// 
+// let n=6;
+// let out="";
+// let sum=0;
+// for(i=1;i<=n;i++){
+//     out+=i+"/"+i+"!";
+//     if(i<n)
+//     {
+//         out+="+"
+//     }
+
+//     // sum=sum*i;
+
+// }
+// console.log(out+"="+sum)
+// let n=6;
+// let out="";
+// let sum=0;
+// for(i=1;i<=n;i+=2){
+//     out+=i+"/"+i+"!";
+//     if(i<n)
+//     {
+//         out+="+"
+//     }
+
+//     // sum=sum*i;
+
+// }
+// // console.log(out)
+// let n=6;
+// let out="";
+// let sum=0;
+// for(i=0;i<=n;i+=2){
+//     out+=i+"/"+i+"!";
+//     if(i<n)
+//     {
+//         out+="+"
+//     }
+
+//     // sum=sum*i;
+
+// }
+// console.log(out)
+
+
+
+// //do while
+
+
+
+// let c= 5;
+// let sum2=0;
+// let out2="";
+// do{
+//     out2+=i;
+//     if(i<n){
+//         out2+="+"
+//     }
+//     sum2+=i;
+//     i++;
+
+// }while(i<=c);
+// console.log(out2+"="+sum2)
+// let i=1;
+// let v=10;
+// let ou="";
+
+// do
+// {
+//     ou+=i;
+//     if(i<v)
+//     {
+//         ou+="+"
+
+//     }
+   
+//     i+=2;
+// }while(i<=10);
+// console.log(ou)
+
+// let i=1;
+// let v=10;
+// let ou="";
+// do{
+//     ou+=i+"/"+i+"!"
+//     if(i<v)
+//     {
+//         ou+="+"
+
+//     }
+//     i++;
+
+// }while(i<=v);
+// console.log(ou)
+
+// 4tw
+
+let i=0;
+let v=10;
+let ou="";
+do{
+    ou+=i+"/"+i+"!"
+    if(i<v)
+    {
+        ou+="+"
+
+    }
+    i+=2;
+
+}while(i<=v);
+console.log(ou)
+
+
+
+//whie loop
+
+// let i=1;
+// let b=5;
+// let out3="";
+// while(i<=b){
+//     out3+=i;
+//     if(i<b){
+//         out3+="+"
+//     }
+//     i++;
+    
+// }
+// console.log(out3)
+
+// let i=1;
+// let b=5;
+// let out3="";
+// while(i<=b){
+//     out3+=i;
+//     if(i<b){
+//         out3+="+"
+//     }
+//     i+=2;
+    
+// }
+// console.log(out3)
+
+// let n=6;
+// let out="";
+// let i=1;
+// while(i<=n){
+//     out+=i+"/"+i+"!";
+//     if(i<n)
+//     {
+//         out+="+"
+//     }
+//     i++;
+
+//     // sum=sum*i;
+
+// }
+// console.log(out)
+
+// let n=6;
+// let out="";
+// let i=0;
+// while(i<=n){
+//     out+=i+"/"+i+"!";
+//     if(i<n)
+//     {
+//         out+="+"
+//     }
+//     i+=2
+
+//     // sum=sum*i;
+
+// }
+// console.log(out)
+
+// let n=6;
+// let out="";
+// let i=0;
+// while(i<=n){
+//     out+=i+"/"+i+"!";
+//     if(i<n)
+//     {
+//         out+="+"
+//     }
+//     i+=2;
+
+//     // sum=sum*i;
+
+// }
+// console.log(out)
